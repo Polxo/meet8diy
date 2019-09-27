a5ab151604b8a1831b74e0b9159e6b759c89055a
frendigeralfernando <frendigeralf@gmail.com>
2019-09-27 21:38:37 +0700

    file tugas pendaftaran

---
+++ resources/views/nando/pendaftarannando.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		.pendaftaran {
+	        margin: 50px auto;
+	        width: 500px;
+	        padding: 10px;
+	        border: 1px solid #ccc;
+	        background: lightblue;
+	     }
+        input , h1 {
+	        margin: 5px auto;
+	        width: 95%;
+	        padding: 10px;
+		}
+	    button { 
+	        float: right;
+	        padding: 5px;
+	        width: 90px;
+	        border: none;
+	        color: #fff;
+	        background: red;
+	        cursor: pointer;
+	    }
+	    
+	</style>
+</head>
+<body>
+	<div class="pendaftaran">
+		<h1 align="center">Input Data Anda</h1>
+		<form action="/pendaftarannando" method="post">
+			@csrf
+			<label>Nama</label>
+		    	<input type="text" name="nama" placeholder="Masukan Nama Lengkap Anda"><br>
+		    <label>Alamat</label>
+		    	<input type="text" name="alamat" placeholder="Masukan Alamat Anda"><br>
+		    <label>No HP</label>
+		    	<input type="number" name="notelp" placeholder="Masukan No HP Anda"><br>
+		    <label>Tgl Lahir</label>
+		    	<input type="date" name="tgllahir" ><br>
+		    <button type="submit">KIRIM</button><br><br>
+		</form>
+	</div>
+	
+</body>
+</html>
